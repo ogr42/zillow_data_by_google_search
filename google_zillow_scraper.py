f4a3e7618e7c20dac1cb57fbfb634da458884351
ogr42 <99117042+ogr42@users.noreply.github.com>
2022-05-15 13:15:58 +0300

    Update google_zillow_scraper.py

--- google_zillow_scraper.py
+++ google_zillow_scraper.py
@@ -1,13 +1,11 @@
-#!pip install beautifulsoup4 
+# Install the BeautifulSoup module using CLI command: pip install beautifulsoup4 
 
 from bs4 import BeautifulSoup
 import csv
 import requests
 
-input_file = 'properties_sample.csv'
-output_file = 'properties_data.csv'
-
 def get_addresses(input_file):
+  # Get a list of addresses from the input file 'properties_sample.csv'
   with open(input_file, 'r') as f:
     read = csv.reader(f, delimiter=',', quotechar=',',
                         quoting=csv.QUOTE_MINIMAL)
@@ -15,6 +13,7 @@ def get_addresses(input_file):
   return address_list[1:]
 
 def queries(addresses):
+  # Create a list of links for a google search
   url_google = 'https://www.google.com/search?q='
   queries = []
   for address in addresses:
@@ -25,11 +24,13 @@ def queries(addresses):
   return queries
 
 def get_soup(query):
+  # Get a google search page for a single address
   resp = requests.get(query, timeout= 3.1)
   soup = BeautifulSoup(resp.text, "html.parser")
   return soup
 
 def property_data(soup):
+  # Parse property data from a google search page
   data = soup.find(class_="ZINbbc luh4tb xpd O9g5cc uUPGi")
   link = data.find('a').get('href').split('=')[1].split('&')[0]
   zpid = link.split('/')[-2].split('_')[0]
@@ -41,27 +42,47 @@ def property_data(soup):
   return home_data
 
 def addr_data(address, data):
-  add_dat = address + data
-  return add_dat
+  # It merges the property data and the property address
+  return address + data
 
 def output_to_file(property_array):
+  # It outputs the property data to the output file 'properties_data.csv'
   with open(output_file, 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(property_array)
 
 def main():
+  # The 'main' function integrares an other functions
   addresses = get_addresses(input_file)
   querie = queries(addresses)
-  property_array = [['build&street', 'city', 'state', 'zip_code', 'link', 'zpid', 'for_sale', 'square feet', 'families', 'beds', 'baths']]
+  # Headers of the table
+  property_array = [['build&street',
+                     'city',
+                     'state',
+                     'zip_code',
+                     'link',
+                     'zpid',
+                     'for_sale',
+                     'square feet',
+                     'families',
+                     'beds',
+                     'baths'
+                    ]] 
   for i, query in enumerate(querie):
     soup = get_soup(query)
     data = property_data(soup)
     data_full = addr_data(addresses[i], data)
     property_array.append(data_full)
-  output_to_file(property_array)
+  output_to_file(property_array) # Output to file
+  # Print output array
   for row in property_array:
     print(row)
+
+#config
+input_file = 'properties_sample.csv'
+output_file = 'properties_data.csv'
 
 if __name__ == "__main__":
   main()
 
+# Author: github.com/ogr42
